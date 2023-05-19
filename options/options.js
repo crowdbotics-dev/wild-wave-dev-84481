5e374d41be038777fa1b2899c4a35b5ebc7dd5ee
Crowdbotics <engineering@crowdbotics.com>
2023-05-19 00:44:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wild-wave-dev-84481",
+  url: "https://wild-wave-dev-84481.botics.co",
+  api: "https://wild-wave-dev-84481.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
